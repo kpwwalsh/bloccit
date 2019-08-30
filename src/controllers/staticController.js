0242d2e0701d027b783326a4013f800f66c7bc22
kpwwalsh <wnuru@hotmail.com>
2019-08-30 12:53:06 -0500

    assignment routing

--- src/controllers/staticController.js
+++ src/controllers/staticController.js
@@ -1,5 +1,8 @@
 module.exports = {
+    about(req, res, next){
+        res.render("static/partials/about", {title: "About Us"});
+    },
     index(req, res, next){
         res.render("static/index", {title: "Welcome to Bloccit"});
     }
-  }
+  };
