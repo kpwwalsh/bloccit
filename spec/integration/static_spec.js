ca52ec506046ec77881399ffaa2da4c248de4b6f
kpwwalsh <wnuru@hotmail.com>
2019-08-30 09:12:56 -0500

    dependency issue checkpoint routing

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,7 +1,6 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
-const marco = "http://localhost:3000/marco";
 
 describe("routes : static", () => {
   describe("GET /", () => {
@@ -9,16 +8,10 @@ describe("routes : static", () => {
     });
 
 describe("GET /", () => {
-    it("should return status code 200", (done) => {
-    request.get(marco, (err, res, body) => {
-        expect(res.statusCode).toBe(200);
-        done();
-      });
-    });
-    it("should return 'polo'", (done) => {
-        request.get(marco, (err, res, body) => {
-          expect(body).toBe('polo');
-          done();
+    it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", (done) => {
+        request.get(base, (err, res, body) => {
+          expect(res.statusCode).toBe(200);
+          expect(body).toContain("Welcome to Bloccit");
    });
   });
 });
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const app = require("./index");
 const http = require("http");
+
 const port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
 
