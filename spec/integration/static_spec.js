4de5f01f000c8fc325facde7af4baac5016ebfdc
kpwwalsh <wnuru@hotmail.com>
2019-08-29 19:58:50 -0500

    assignment intro express complete

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,23 +1,26 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
+const marco = "http://localhost:3000/marco";
 
 describe("routes : static", () => {
+  describe("GET /", () => {
+      });
+    });
+ 
 
-//#1
-  describe("GET /marco", () => {
-
-//#2
+describe("GET /", () => {
     it("should return status code 200", (done) => {
-
-//#3
-      request.get(base, (err, res, body) => {
+      request.get(marco, (err, res, body) => {
         expect(res.statusCode).toBe(200);
-
-//#4
         done();
       });
     });
 
+    it("should return 'polo'", (done) => {
+      request.get(marco, (err, res, body) => {
+        expect(body).toBe('polo');
+        done();
+      });
+    });
   });
-});
