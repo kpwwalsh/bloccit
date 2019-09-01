69f218f30210ee67d413cd6cd3877a28b09ec622
kpwwalsh <wnuru@hotmail.com>
2019-08-31 19:48:56 -0500

    assignment: routing controllers view

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -4,10 +4,6 @@ const base = "http://localhost:3000/";
 
 describe("routes : static", () => {
   describe("GET /", () => {
-      });
-    });
-
-describe("GET /", () => {
     it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", (done) => {
         request.get(base, (err, res, body) => {
           expect(res.statusCode).toBe(200);
@@ -15,3 +11,15 @@ describe("GET /", () => {
    });
   });
 });
+
+describe("GET /about", () => {
+    it("should return status code 200 and string 'About Us' in the body of the response", (done) => {
+      request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(body).toContain("About Us");
+        done();
+      });
+    });
+  });
+});
+ 
