675be4a52aeb99ec626a0ff023d4e86651f9ac58
kpwwalsh <wnuru@hotmail.com>
2019-10-01 10:11:57 -0500

    checkpoint voting complete

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -6,6 +6,7 @@ init(app){
   const postRoutes = require("../routes/posts");
   const userRoutes = require("../routes/users");
   const commentRoutes = require("../routes/comments");
+  const voteRoutes = require("../routes/votes");
 
   if(process.env.NODE_ENV === "test") {
     const mockAuth = require("../../spec/support/mock-auth.js");
@@ -17,5 +18,6 @@ init(app){
   app.use(postRoutes);
   app.use(userRoutes);
   app.use(commentRoutes);
+  app.use(voteRoutes);
    }
   }
